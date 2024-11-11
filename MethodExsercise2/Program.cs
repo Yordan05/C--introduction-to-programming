15b9eb598241bd1f36c6aacf634a7a8df018cf9b
Yordan05 <jor95003@gmail.com>
2024-11-11 12:25:12 +0200

    Add files via upload

---
+++ MethodExsercise2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace MethodExsercise2
+{
+    internal class Program
+    {
+            static void Main(string[] args)
+            {
+
+
+                int a = int.Parse(Console.ReadLine());
+                int b = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("The bigger number is" + (GetMax(a, b)));
+            }
+            static int GetMax(int a, int b)
+            {
+                
+                
+
+                return Math.Max(a, b);
+
+
+
+
+
+
+            }
+        }
+    }
---
+++ meThods exercise/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace meThodsexercise
+{
+    class Program
+    {
+
+        static void Method(string name)
+        {
+            Console.WriteLine($"Hello, {name}!");
+        }
+
+
+            static void Main(string[] args)
+            {
+
+                Console.Write("Enter your name: ");
+
+                string name = Console.ReadLine();
+
+            Method(name);   
+
+
+            }
+        }
+    }
